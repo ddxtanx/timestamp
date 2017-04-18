784d268ee5d05f610cbd1cf299e6625a2b332b6a
Garrett Credi <gcc@ameritech.net>
2017-04-18 17:46:58 +0000

    Adding timestamp features

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var app = express();
 
-app.get("/", function(req, res){
-    res.end("hello world");
-});
-
+app.get("/:date", function(req, res){
+    var date = req.params.date;
+    var parsedDate = Date.parse(date);
+    var dateConstructed = new Date(parsedDate);
+    res.writeHead(200, {'Content-Type': 'text/json'});
+    if(!isNaN(parsedDate)){
+        var data = {
+            "unix": parsedDate/1000,
+            "natural": dateConstructed.toDateString()
+        }
+        data = JSON.stringify(data);
+        res.end(data);
+    } else{
+        var data = {};
+        data = JSON.stringify(data);
+        res.end(data);
+    }
+})
 app.listen(8080);
